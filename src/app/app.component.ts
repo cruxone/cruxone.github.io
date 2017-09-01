72841542608c9164f5a33fb1a30052bb3a03045c
Chris Freeland <diathis47@gmail.com>
2017-08-31 18:26:33 -0700

    yup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  showHide: boolean;
+  title = 'CruxHome';
+  constructor() {
+    this.showHide = false;
+  }
+  changeShowStatus(){
+    this.showHide = !this.showHide;
+  }
 }
